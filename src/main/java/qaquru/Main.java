4de5ec3f950b98b956c34c8443dc23e36af78c48
Elmira Shaykhattarova <elmirailgizovna@gmail.com>
2023-09-12 23:52:21 +0300

    added new message

--- src/main/java/qaquru/Main.java
+++ src/main/java/qaquru/Main.java
@@ -2,6 +2,11 @@ package qaquru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa guru!");
+        System.out.println(getMessage());
     }
+
+
+public static String getMessage() {
+    return "hello qa guru";
+}
 }
