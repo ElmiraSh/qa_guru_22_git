c76b903d6de4b9b096bec4f674ca72fc291bc4a2
ElmiraSh <elmirailgizovna@gmail.com>
2023-09-13 19:08:17 +0300

    Update Main.java

--- src/main/java/qaquru/Main.java
+++ src/main/java/qaquru/Main.java
@@ -1,6 +1,8 @@
 package qaquru;
 
 public class Main {
+    private static String msg="hello world";
+    
     public static void main(String[] args) {
         System.out.println("Hello, qa guru1!");
     }
